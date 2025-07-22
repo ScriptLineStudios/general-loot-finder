8131bfb7e9b5294d6dc8a5691b53663dd427c3fd
ScriptLineStudios <scriptlinestudios@protonmail.com>
2025-07-22 20:54:24 +0200

    build folder

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # big thank you to dutchen and kris, who helped me get this working :)
 set -e
-nvcc -O3 -g -I src/include --device-c src/loot_library.cu -o loot_library.o -DUSE_CUDA
-nvcc -O3 -g -I src/include --device-c src/loot_data.cu -o loot_data.o -DUSE_CUDA
-nvcc -O3 -g -I src/include --device-c example.cu -o example.o -DUSE_CUDA
-nvcc -O3 -g *.o cJSON/libcjson.a -o example 
+mkdir build
+nvcc -O3 -g -I src/include --device-c src/loot_library.cu -o build/loot_library.o -DUSE_CUDA
+nvcc -O3 -g -I src/include --device-c src/loot_data.cu -o build/loot_data.o -DUSE_CUDA
+nvcc -O3 -g -I src/include --device-c example.cu -o build/example.o -DUSE_CUDA
+nvcc -O3 -g *.o cJSON/libcjson.a -o build/example 
